479b6458d011e8af9c6147e7ad966581eb839d76
Patrick Slattery <slattery.patrickj@gmail.com>
2013-10-26 16:42:23 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'em-synchrony'
+gem 'slim-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'json'
+gem 'jquery-rails'
+
+
+# MongoDB
+gem 'bson_ext'
+gem 'mongoid'
+
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl'
+  gem 'mongoid-rspec'
+end
+
+group :test do
+  gem 'jasminerice'
+  gem 'capybara'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ukelele::Application.config.secret_token = '18ce56101d81082c2fb7022b62a325b328bdf002de6cafe4e2bc41802cca3025f6f4098998b0dd146028454ea32364f3dcdc5a1688c3f46fee5900b17da8d352'
